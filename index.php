67791efc572939064aadfe87abc06915185e2e33
krishnaguragain <krishnaguragain@users.noreply.github.com>
2014-04-09 09:06:31 -0700

    Update index.php
    adding array of month ,rashi,day,tithi,karan,yoga in english format

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <?php
 
 $unixtime = time(); //$date_to_unixtime = strtotime('25th October 2009 11:12:34 PM (UTC)');
-
+// var_dump($unixtime);
 // deg2rad(45);
 function rad2deg($arg) {
  return $arg*180/pi();
 }
-var_dump($unixtime);
+$enmonth=array(1 => "January","February","March","April","May","June",
+		  "July","August","September","October","November","December");
+		  
+$enrashi=array("Mesha","Vrishabha","Mithuna","Karka","Simha","Kanya","Tula",
+		   "Vrischika","Dhanu","Makara","Kumbha","Meena");	  
+$enday =array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
 
+$entithi = ("Prathame","Dwithiya","Thrithiya","Chathurthi","Panchami",
+		  "Shrashti","Saptami","Ashtami","Navami","Dashami","Ekadashi",
+		  "Dwadashi","Thrayodashi","Chaturdashi","Poornima","Prathame",
+		  "Dwithiya","Thrithiya","Chathurthi","Panchami","Shrashti",
+		  "Saptami","Ashtami","Navami","Dashami","Ekadashi","Dwadashi",
+		  "Thrayodashi","Chaturdashi","Amavasya");
+
+$enkaran=("Bava","Balava","Kaulava","Taitula","Garija","Vanija",
+		  "Visti","Sakuni","Chatuspada","Naga","Kimstughna");
+
+$enyoga = ("Vishkambha","Prithi","Ayushman","Saubhagya","Shobhana",
+		 "Atiganda","Sukarman","Dhrithi","Shoola","Ganda","Vridhi",
+		 "Dhruva","Vyaghata","Harshana","Vajra","Siddhi","Vyatipata",
+		 "Variyan","Parigha","Shiva","Siddha","Sadhya","Shubha","Shukla",
+		 "Bramha","Indra","Vaidhruthi");
 ?>
