171e99d94b42fc17a1fa11cd2f0de36b5661b68a
krishnaguragain <krishnaguragain@users.noreply.github.com>
2014-04-09 08:59:03 -0700

    Update index.php
    add deg2rad and rad2degree

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 $unixtime = time(); //$date_to_unixtime = strtotime('25th October 2009 11:12:34 PM (UTC)');
 
+// deg2rad(45);
+function rad2deg($arg) {
+ return $arg*180/pi();
+}
 var_dump($unixtime);
 
 ?>
